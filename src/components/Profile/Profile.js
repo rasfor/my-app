d6edfbf95ccb5340da81d75c76fdf6a12df9f619
zulfiya <zulfiya.khanova@saprun.com>
2022-06-08 13:29:50 +0300

    some changes in Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,8 +4,8 @@ import Myposts from '../Myposts/Myposts';
 
 const Profile = () => {
   return (
-    <div className={module.profile}>
-      <img src='https://avatars.mds.yandex.net/get-zen_doc/1708007/pub_6043694a665e4413f3c4ad55_6043ad4d58285736ddaf8c1e/scale_1200' />
+    <div>
+      <img className={module.ava} src='https://avatars.mds.yandex.net/get-zen_doc/1708007/pub_6043694a665e4413f3c4ad55_6043ad4d58285736ddaf8c1e/scale_1200' />
       <Myposts />
     </div>
   )
