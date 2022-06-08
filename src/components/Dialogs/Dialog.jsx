d0dba810a15b53429b3289c9520bb00c00fdbde2
zulfiya <zulfiya.khanova@saprun.com>
2022-06-08 17:28:01 +0300

    add dialogs

--- src/components/Dialogs/Dialog.jsx
+++ src/components/Dialogs/Dialog.jsx
@@ -2,9 +2,20 @@ import module from './Dialog.module.css'
 
 const Dialog = () => {
   return (
-    <div>
-      Dialogs
+    <div className={module.dialogs}>
+      <div className={module.contacts}>
+        <div className={module.contact + ' ' + module.active}>Zulya</div>
+        <div className={module.contact}>Timur</div>
+        <div className={module.contact}>Bakha</div>
+        <div className={module.contact}>Gulshatik</div>
+      </div>
+      <div className={module.messages}>
+        <div className={module.message}>Hello</div>
+        <div className={module.message}>What's up?</div>
+        <div className={module.message}>To be honest, I love you</div>
+      </div>
     </div>
+
   );
 }
 
