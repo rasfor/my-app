a02912e302cb63fd22ddaec1d042b7a1de39a86e
zulfiya <zulfiya.khanova@saprun.com>
2022-06-09 15:15:18 +0300

    размножаем объекты с помощью ф-ии map

--- src/components/Dialogs/Dialog.jsx
+++ src/components/Dialogs/Dialog.jsx
@@ -3,21 +3,37 @@ import module from './Dialog.module.css';
 import Contact from './Contact/Contact';
 import Message from './Message/Message';
 
+let contacts = [
+  { id: 1, name: 'Hinata' },
+  { id: 2, name: 'Sasuke' },
+  { id: 3, name: 'Sakura' },
+  { id: 4, name: 'Kakashi' }
+]
+
+let messages = [
+  { id: 1, messageText: 'Hello' },
+  { id: 2, messageText: 'Yo' },
+  { id: 3, messageText: 'Watashi backa' },
+  { id: 4, messageText: 'kek' },
+  { id: 5, messageText: 'chebureck' }
+]
+
+let contactElements = contacts.map((contact) => {
+  return <Contact id={contact.id} name={contact.name} />
+})
+
+let messageElements = messages.map((message) => {
+  return <Message id={message.id} messageText={message.messageText} />
+})
+
 const Dialog = () => {
   return (
     <div className={module.dialogs}>
       <div className={module.contacts}>
-        <Contact id='1' name='Kakashi' />
-        <Contact id='2' name='Sasuke' />
-        <Contact id='3' name='Sakura' />
-        <Contact id='4' name='Hinata' />
+        {contactElements}
       </div>
       <div className={module.messages}>
-        <Message messageText='Hello' />
-        <Message messageText='Yo' />
-        <Message messageText='Watashi backa' />
-        <Message messageText='kek' />
-        <Message messageText='chebureck' />
+        {messageElements}
       </div>
     </div>
 
