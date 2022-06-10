d8dd639100952f89d253631c6724450645c104a3
zulfiya <zulfiya.khanova@saprun.com>
2022-06-10 12:04:11 +0300

    replace MyPosts folder into profile folder

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import module from './Profile.module.css'
-import Myposts from '../Myposts/Myposts';
+import Myposts from './Myposts/Myposts';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = (props) => {
