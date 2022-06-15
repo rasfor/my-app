6a206fdb100012301d68efe5b0eb733e6696d064
Zulfiya <zulfiya.khanova@saprun.com>
2022-06-15 11:12:03 +0300

    add Ref, createRef

--- src/components/Profile/Myposts/Newpost/Newpost.js
+++ src/components/Profile/Myposts/Newpost/Newpost.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import module from './Newpost.module.css'
 
 const Newpost = () => {
+
+  let newPostText = React.createRef();
+
+  let addPost = ()=>{
+    debugger;
+    let postText = newPostText.current.value;
+    alert(postText);
+  }
+
   return (
     <div className={module.newPost}>
       <div>
-        <textarea></textarea>
+        <textarea ref={newPostText}></textarea>
       </div>
       <div>
-        <button>Add post</button>
+        <button onClick={addPost}>Add post</button>
       </div>
     </div>
   );
