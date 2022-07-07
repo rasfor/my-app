258e869892db9004fd1861440a53bfdcdbd69d9e
rasfor <nekotanekota@yandex.ru>
2022-07-07 14:00:24 +0300

    71 урок local state setState

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -1,15 +1,17 @@
-import module from './ProfileInfo.module.css'
+import module from './ProfileInfo.module.css';
+import Status from '../Status/Status';
 
 const ProfileInfo = (props) => {
   return (
     <div className={module.currentUser}>
-      <img 
-        src={props.profileData.photos.large!=null ?
-          props.profileData.photos.large:
+      <img
+        src={props.profileData.photos.large != null ?
+          props.profileData.photos.large :
           'https://avatars.mds.yandex.net/get-zen_doc/1708007/pub_6043694a665e4413f3c4ad55_6043ad4d58285736ddaf8c1e/scale_1200'} />
       <div>
         {props.profileData.fullName}
       </div>
+      <Status status="Hello It kamasuta!" />
     </div>
   )
 }
