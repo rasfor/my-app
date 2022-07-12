e99752b350d5fd67c8e8c0f642f5d722f7ce49b4
rasfor <nekotanekota@yandex.ru>
2022-07-12 10:30:39 +0300

    74 урок исправили рассинхронщину со статусом use componentDidUpdate

--- src/components/Profile/Status/Status.js
+++ src/components/Profile/Status/Status.js
@@ -2,6 +2,14 @@ import React from "react";
 
 class Status extends React.Component {
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(prevProps.status != this.props.status) {
+      this.setState({
+        status: this.props.status
+      })
+    }
+  }
+
   state = {
     editMode: false,
     status: this.props.status
